f39ed3275b00d83886081af7a6eb493bedd1f3e5
vidushan <vidushanpathir505@gmail.com>
2026-04-17 19:59:55 +0530

    Add UI to the game

--- tic-tac-toe-v2/gui.py
+++ tic-tac-toe-v2/gui.py
@@ -5,9 +5,18 @@ from tkinter import messagebox
 class GameEngine:
 
     def __init__(self, root):
+
         self.root = root
         self.root.title("Tic-Tac-Toe")
         self.root.geometry('500x550')
+
+        self.bg_color = "#1e1e2f"
+        self.btn_color = "#2d2d44"
+        self.x_color = "#ff4d4d"
+        self.o_color = "#4da6ff"
+        self.text_color = "#ffffff"
+
+        self.root.configure(bg=self.bg_color)
 
         self.game_start()
 
@@ -15,11 +24,28 @@ class GameEngine:
 # This is the openning screen of the app
         self.clear_screen()
 
-        first_screen = tk.Frame(self.root)
+        first_screen = tk.Frame(self.root, bg=self.bg_color)
 
-        tk.Label(first_screen, text= 'Tic-Tac-Toe', font=('Arial', 40, 'bold'), padx=10, pady=20).pack()
+        tk.Label(
+            first_screen, 
+            text= 'Tic-Tac-Toe',
+            font=('Arial', 40, 'bold'), 
+            fg=self.text_color,
+            bg=self.bg_color
+            ).pack(pady=30)
 
-        tk.Button(first_screen, text='PLAY', font=('Arial', 20), command=self.playing_mode, padx=10, pady=10).pack(pady=(100, 0))
+        tk.Button(
+            first_screen,
+            text='PLAY', 
+            font=('Arial', 20),
+            bg=self.btn_color,
+            fg=self.text_color,
+            activebackground='#444',
+            relief='flat',
+            command=self.playing_mode, 
+            padx=20, 
+            pady=10
+            ).pack(pady=80)
 
         first_screen.pack(expand=True, fill='both')
 
@@ -34,11 +60,42 @@ class GameEngine:
 
         self.clear_screen()
 
-        second_screen = tk.Frame(self.root)
+        second_screen = tk.Frame(self.root, bg=self.bg_color)
 
-        tk.Button(second_screen, text='PVP', font=('Arial', 20), command=lambda: self.start_play('pvp'), padx=10, pady=10).pack(pady=(100,50))
+        tk.Label(
+            second_screen,
+            text="Choose Mode",
+            font=('Arial', 28, 'bold'),
+            fg=self.text_color,
+            bg=self.bg_color
+            ).pack(pady=40)
 
-        tk.Button(second_screen, text='BOT', font=('Arial', 20), command=lambda: self.start_play('bot'), padx=10, pady=10).pack()
+
+        tk.Button(
+            second_screen, 
+            text='PVP', 
+            font=('Arial', 16, 'bold'), 
+            bg=self.btn_color,
+            fg=self.text_color,
+            activebackground="#444",
+            relief="flat",
+            command=lambda: self.start_play('pvp'), 
+            padx=20, 
+            pady=10
+            ).pack(pady=20)
+
+        tk.Button(
+            second_screen, 
+            text='BOT', 
+            font=('Arial', 16, 'bold'), 
+            command=lambda: self.start_play('bot'),
+            bg=self.btn_color,
+            fg=self.text_color,
+            activebackground="#444",
+            relief="flat", 
+            padx=20, 
+            pady=10
+            ).pack(pady=20)
 
         second_screen.pack(expand=True, fill='both')
 
@@ -63,16 +120,21 @@ class GameEngine:
         for i in range(9): 
             btn = tk.Button(
                 third_screen,
-                font=('Arial', 15, 'bold'),
+                font=('Arial', 24, 'bold'),
                 text="",
-                width=12,
-                height=6,
-                command=lambda i=i: self.on_button_click(i))
+                width=4,
+                height=2,
+                bg=self.btn_color,
+                fg=self.text_color,
+                activebackground='#555',
+                relief='flat',
+                command=lambda i=i: self.on_button_click(i)
+                )
 
             row = i // 3
             col = i % 3
 
-            btn.grid(row=row, column=col)
+            btn.grid(row=row, column=col, padx=5, pady=5)
 
             self.buttons.append(btn)
         
@@ -115,9 +177,11 @@ class GameEngine:
             value = self.game.board[i]
 
             if value == '_':
-                self.buttons[i].config(text='')
+                self.buttons[i].config(text='', state='normal')
+            elif value == 'X':
+                self.buttons[i].config(text='X', fg=self.x_color, state='disable')
             else:                    
-                self.buttons[i].config(text=value)
+                self.buttons[i].config(text='O', fg=self.o_color, state='disable')
 
     def restart_game(self):
 # restart the game
@@ -128,7 +192,5 @@ class GameEngine:
 
         self.update_board()
 
-root = tk.Tk()
-app = GameEngine(root)
-root.mainloop()
+
         
